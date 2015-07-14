3afa65187f543c32db5b3a902f5735cb012c9e9a
Michael Champlin <mjchamplin@gmail.com>
2015-07-14 01:17:11 -0400

    extra semicolon

--- assets/php/header.php
+++ assets/php/header.php
@@ -3,7 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="/assets/libs/responsive-nav.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic">;
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic">
 	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="/assets/libs/responsive-nav.min.js"></script>
     <script src="/assets/libs/textFit.min.js"></script>
